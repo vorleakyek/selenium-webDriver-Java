72fb6fc2e828631ecea60b0ccd55da60969ac037
Vorleak Yek <vorleakyek@gmail.com>
2025-04-03 20:54:34 -0700

    Create webDriver-intro.java

---
+++ webDriver-intro.java
@@ -0,0 +1,23 @@
+package setup;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import static io.github.bonigarcia.wdm.WebDriverManager.chromedriver;
+
+public class Main {
+  static protected WebDriver driver;
+
+  static {
+    chromedriver().setup();
+    var options = new ChromeOptions();
+    options.addArguments("--no-sandbox");
+    driver = new ChromeDriver(options);
+  }
+
+  public static void main(String[] args) {
+    driver.get("https://google.com");
+    driver.quit(); 
+  }
+}
